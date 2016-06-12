d7015d2157aaa0193d628e673e4d39970aa81696
Léo Gaspard <leo@gaspard.io>
2016-06-12 16:25:24 +0200

    Allow compiling on OpenBSD
    Thanks rain1 for the suggestion for this patch. Unfortunately, it came bundled
    with other changes which couldn't be accepted, so here is the import-able part.

--- config.mk
+++ config.mk
@@ -5,9 +5,10 @@ WARN_CFLAGS = -Wall -Wextra -pedantic -D_XOPEN_SOURCE=700
 DBG_CFLAGS  = -g
 DBG_LDFLAGS = -g
 
-FT_CFLAGS  = $(shell freetype-config --cflags)
-FT_LDFLAGS = $(shell freetype-config --libs)
+FT_CFLAGS  = `freetype-config --cflags`
+FT_LDFLAGS = `freetype-config --libs`
 
+XLIB_CFLAGS  = -I/usr/X11R6/include/
 XLIB_LDFLAGS = -lX11 -lXrender
 
 CFLAGS  += -std=c99 ${XLIB_CFLAGS} ${FT_CFLAGS} ${WARN_CFLAGS} ${DBG_CFLAGS}
